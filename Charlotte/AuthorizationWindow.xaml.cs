c727b76c315bb0384c0c5c70c2036869a050651b
steelbsteel <mrtrecer@bk.ru>
2023-04-04 10:46:10 +0300

    first commit

---
+++ Charlotte/AuthorizationWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Charlotte
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource =
+                new BitmapImage(new Uri("C:\\Users\\user\\source\\repos\\Charlotte\\Charlotte\\Images\\AuthBackground.jpg", UriKind.Absolute));
+            this.Background = myBrush;
+        }
+    }
+}
---
+++ Charlotte/DateBase/User.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charlotte.DateBase
+{
+    internal class User
+    {
+        [BsonIgnoreIfNull]
+        public string Login { get; set; }
+        [BsonIgnoreIfNull]
+        public string Password { get; set; }
+        [BsonIgnoreIfNull]
+        public string Email { get; set; }
+        [BsonIgnoreIfNull]
+        public string PhoneNumber { get; set; }
+        [BsonIgnoreIfNull]
+        public string Role { get; set; }
+        [BsonIgnoreIfNull]
+        public byte[] Photo { get; set; }
+        [BsonIgnore]
+        public User currentUser;
+    }
+}
